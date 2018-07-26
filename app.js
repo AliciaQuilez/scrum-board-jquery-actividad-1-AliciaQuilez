e7abbb15cb8379ac2b8d6687f3513226b4de9c66
Alicia Quílez <aliciaquilezsayas@gmail.com>
2018-07-26 17:01:46 +0200

    Código clonado comentado

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 $( document ).ready( function () {
+    // cogemos el input del header
     let addListInput = $( '.addListWrapper input' );
+    // genera un id para cada lista
     const generateId = namespace => `${namespace}-${Date.now()}-${Math.ceil(Math.random()*100)}`
+    // función que contiene el string de creación de una lista
     const createListString = name =>
         `<div class="list" id="${generateId('list')}">
             <div class="listHeader">
@@ -8,13 +11,15 @@ $( document ).ready( function () {
                 <button>X</button>
             </div>
             <div class="tasks">
-                
+
             </div>
             <div class="addTask">
                 <input type="text">
                 <button>Add task</button>
             </div>
         </div>`
+
+    // función que añade una lista a la section ".lists"
     const appendNewList = () => {
          //  cogemos el text del input
          let listName = addListInput.val();
@@ -32,11 +37,12 @@ $( document ).ready( function () {
 
     // Listeners
      addListInput.on( 'keyup', function ( event ) {
+       // cuando se clicka el botón enter --> code 13
         if ( event.keyCode === 13 ) {
            appendNewList();
         }
     } )
-
+    // evento delegado para cuando se cree el listHeader button
      $('.lists').on('click', '.listHeader button', function(event) {
         let listNode = $(event.target.parentNode.parentNode);
         listNode.detach();
