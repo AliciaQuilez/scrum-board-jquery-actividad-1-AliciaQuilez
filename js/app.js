ae6c17c401c156127f115818a21e195c75feec6f
Alicia Quílez <aliciaquilezsayas@gmail.com>
2018-07-31 16:08:58 +0200

    boton de añadir nuevo

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
   // cogemos el input del header
   let addListInput = $('.addListWrapper input');
+  let addListButton = $('.addListWrapper a');
   // genera un id para cada lista
   const generateId = namespace => `${namespace}-${Date.now()}-${Math.ceil(Math.random()*100)}`
   // función que contiene el string de creación de una lista
@@ -8,11 +9,12 @@ $(document).ready(function() {
     `<div class="list indigo darken-1" id="${generateId('list')}">
             <div class="listHeader">
                 <h4>${name}</h4>
-                <a class="waves-effect waves-teal btn-flat"><i class="material-icons">close</i></a>
+                <a class="waves-effect waves-teal btn-flat"><i class="material-icons">clear</i></a>
             </div>
             <div class="tasks"></div>
             <div class="addTask">
                 <input type="text" placeholder="Add task">
+                <a class="waves-effect waves-teal btn-flat add"><i class="material-icons">add</i></a>
             </div>
         </div>`
 
@@ -20,9 +22,13 @@ $(document).ready(function() {
   const appendNewList = () => {
     //  cogemos el text del input
     let listName = addListInput.val();
-
+    //Nombre por defecto de la lista si el valor es ''
+    if (listName === ''){
+      listName = 'New List';
+    }
     // creamos el nodo .list
     let list = $(createListString(listName));
+
 
     // añadimos el node al DOM
     $('.lists').append(list)
@@ -37,14 +43,17 @@ $(document).ready(function() {
   let createTaskString = taskName =>
     `<div class="task card-panel">
         <div class="text">${taskName}</div>
-        <a class="waves-effect waves-teal btn-flat"><i class="material-icons">close</i></a>
+        <a class="waves-effect waves-teal btn-flat"><i class="material-icons">clear</i></a>
     </div>`
 
   // función para append la nueva task pasamos el nodo donde crea y el input que hemos rellenado
   const appendNewTask = (tNode, tInput) => {
     // cogemos el valor del input de task
     let taskInputValue = tInput.val();
-
+    //Nombre por defecto de la lista si el valor es ''
+    if (taskInputValue === ''){
+      taskInputValue = 'New Task';
+    }
     // añadimos a task el string que crea el html de task
     let task = $(createTaskString(taskInputValue));
 
@@ -56,6 +65,7 @@ $(document).ready(function() {
   }
 
   // Listeners
+
   addListInput.on('keyup', function(event) {
     // cuando se clicka el botón enter --> code 13
     if (event.keyCode === 13) {
@@ -63,16 +73,21 @@ $(document).ready(function() {
 
     }
   })
+  //añadir lista clickando en el botón
+  addListButton.on('click', function(event) {
+      appendNewList();
+  })
+
 
   // evento delegado para borrar lista y afecte al botón creado despues del .document.ready
   $('.lists').on('click', '.listHeader a', function(event) {
     let listNode = $(event.target.parentNode.parentNode.parentNode);
-    setTimeout(function() {
+
       listNode.detach();
-    }, 600);
+
   })
 
-  //delegate event para añadir tareas
+  //delegate event para añadir tareas con enter
   $('.lists').on('keyup', '.list .addTask input', function(event) {
     if (event.keyCode === 13) {
       //guarda el nodo ".tasks"
@@ -89,9 +104,6 @@ $(document).ready(function() {
     console.log($(event.target));
     let tasksNode = $(event.target.parentNode.parentNode);
     console.log(tasksNode);
-    setTimeout(function() {
     tasksNode.detach();
-    }, 600);
-
   })
 })
